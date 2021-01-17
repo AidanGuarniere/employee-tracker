e18165cbd4fdfc89ac77d82ff03a79dee32df213
aidan <aidanguarniere@gmail.com>
2021-01-17 14:04:46 -0800

    foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -2,26 +2,32 @@ DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 USE company_db;
 
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (department_id)
+);
+
+CREATE TABLE roles (
+    role_id INT AUTO_INCREMENT NOT NULL,   
+    title VARCHAR(40) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (role_id),
+    CONSTRAINT fk_departments
+    FOREIGN KEY (department_id)
+    REFERENCES departments(department_id)
+    ON DELETE SET NULL
+);
+
 CREATE TABLE employees (
     id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    -- role_id INT NOT NULL,
-    PRIMARY KEY (id)
-    -- FOREIGN KEY (role_id)
-    -- REFERENCES roles(role_id)
+    role_id INT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_roles
+    FOREIGN KEY (role_id)
+    REFERENCES roles(role_id)
+    ON DELETE SET NULL
 );
-
--- CREATE TABLE roles (
---     role_id INT AUTO_INCREMENT NOT NULL,   
---     title VARCHAR(40) NOT NULL,
---     salary DECIMAL NOT NULL,
---     department_id INT,
---     PRIMARY KEY (role_id)
--- );
-
--- CREATE TABLE departments (
---     id INT AUTO_INCREMENT NOT NULL,
---     name VARCHAR(30),
---     PRIMARY KEY (id)
--- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,22 @@
-INSERT INTO employees (first_name, last_name)
+INSERT INTO departments (name)
 VALUES
-    ('John', 'Doe'),
-    ('Jeff', 'Dat'),
-    ('Pete', 'Dubrowski'),
-    ('Hal', 'Jordan');
+    ('Law'),
+    ('Marketing'),
+    ('Sales'),
+    ('Engineering');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Head of Law', 600000, 1),
+    ('Regional Manager of Marketing', 240000, 2),
+    ('Regional Manager of Sales', 265000, 3),
+    ('Head of Engineering', 478000, 4);
+
+
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+    ('John', 'Doe', 1),
+    ('Jeff', 'Dat', 2),
+    ('Pete', 'Dubrowski', 3),
+    ('Hal', 'Jordan', 4);
